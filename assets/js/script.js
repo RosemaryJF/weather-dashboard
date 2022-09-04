11fb926181a99bb15562071facae5ae47c94d3ee
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-09-04 12:19:45 +1000

    City search function and local storage set user entry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,48 @@
 var APIKey = "f39d9467ef1aba260c6801b3458f906e";
-var city;
+// var city;
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
 
-fetch(queryURL)
+// fetch(queryURL)
+
+var searchFormEl = document.getElementById("city-search");
+
+// function citySearch (event) {
+
+//     var city = document.querySelector("#user-input").value;
+    
+
+//     searchFormEl.addEventListener("submit", citySearch);
+//     event.preventDefault();
+// }
+// citySearch()
+$(document).ready(function(citySearch){
+
+    $("form").submit(function (event) {
+        event.preventDefault();
+        var userCityEntry = $("#user-input").val()
+        console.log(userCityEntry);
+        localStorage.setItem("City Search", userCityEntry);
+    });
+
+    citySearch()
+})
+
+
+
+
+
+// $( "#city-search" ).submit(function( event ) {
+//     event.preventDefault();
+//     var userCityEntry = $(this).siblings(".user-input").val();
+//         userCityEntry = $.trim(userCityEntry);
+//         console.log(userCityEntry);
+//         console.log(citySearch);
+
+//         localStorage.setItem("City Search", userCityEntry);
+//   });
+
+
+
+
+  
