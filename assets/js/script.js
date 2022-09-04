79dc3bcdca3ef742e9b0aefe88d8281ab0f7c838
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-09-04 10:46:02 +1000

    Search input and button creation. Plus beginning of fetch call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-var APIkey = "f39d9467ef1aba260c6801b3458f906e";
+var APIKey = "f39d9467ef1aba260c6801b3458f906e";
+var city;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
+
+fetch(queryURL)
