c56430977996aefa8a262f8ca12097a519f5ed72
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-09-11 10:40:14 +1000

    Weather icon addition to current day result, JS + HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ function citySearch () {
 citySearch()
 
 
-    
+// Function to retrieve current day weather for user city search
 function returnResultsCurrentDay () {
     userCityEntry = $("#user-input").val().toLowerCase()
     var queryURL =  "https://api.openweathermap.org/data/2.5/weather?q=" + userCityEntry + "&appid=" + APIKey + "&units=metric&temperature.value=&wind.speed=&humidity.value=&current.uvi=";
@@ -39,20 +39,35 @@ function returnResultsCurrentDay () {
             console.log(data);
 
             var cityName = data.name;
+            
             var temperature = data.main.temp;
             var wind = data.wind.speed;
             var humidity = data.main.humidity;
             var uvIndex = data.uvi;
-            var weatherIcon = data.weather[0].icon;
+            var weatherIconCode = data.weather[0].icon;
+            var weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIconCode + ".png"
             var date = moment().format(" (DD/MM/YYYY) ");
 
         
-            $("#results-display").append(cityName + date + weatherIcon);
-            
+            $("#results-display").append(cityName + date);
+            $("#weather-icon").attr("src", weatherIconURL);
             $("#temperature").append("Temp: " + temperature);
             $("#wind").append("Wind: " + wind);
             $("#humidity").append("Humidity: " + humidity);
             $("#uv-index").append("UV Index: " + uvIndex);
+
+            var longitude = data.coord.lon;
+            var latitude = data.coord.lat;
+
+            var uvIndexURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lon=" + longitude + "&lat=" + latitude + APIKey +"&cnt=1";
+            fetch(uvIndexURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                })
+
         })
 }
 
