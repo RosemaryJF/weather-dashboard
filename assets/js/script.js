6bc25459e3cded3dda4a9c71a1cf39dfdab0a523
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-09-16 20:23:32 +1000

    Hiding result containers and function to remove. Got UV index call to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,81 +7,84 @@ var resultsDisplayEl = $(".results-display")
 
 
 // $(document).ready(function(citySearch){
-function citySearch () {
+function citySearch() {
     $("form").submit(function (event) {
         event.preventDefault();
         userCityEntry = $("#user-input").val()
         console.log(userCityEntry);
         localStorage.setItem("City Search", userCityEntry);
 
-        
-        $("#search-history").text(userCityEntry);
+        $("#current-day-result").removeAttr("style");
+        $("#five-day-result").removeAttr("style");
         returnResultsCurrentDay()
         returnResultsFiveDay()
         returnFiveDayDate()
+        $("#search-history").text(userCityEntry);
         return
     })
-    
+
 }
 citySearch()
 
 
 // Function to retrieve current day weather for user city search
-function returnResultsCurrentDay () {
+function returnResultsCurrentDay() {
     userCityEntry = $("#user-input").val()
-    var queryURL =  "https://api.openweathermap.org/data/2.5/weather?q=" + userCityEntry + "&appid=" + APIKey + "&units=metric";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCityEntry + "&appid=" + APIKey + "&units=metric";
     // &temperature.value=&wind.speed=&humidity.value=&current.uvi="
 
     fetch(queryURL)
         .then(function (response) {
-                return response.json();
-            })
+            // if (!response.ok) {
+            return response.json();
+            // }
+        })
 
-        .then (function(data) {
-
+        .then(function (data) {
             console.log(data);
 
             var cityName = data.name;
-            
+
             var temperature = data.main.temp;
             var wind = data.wind.speed;
             var humidity = data.main.humidity;
-            var uvIndex = data.uvi;
             var weatherIconCode = data.weather[0].icon;
             var weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIconCode + ".png"
             var date = moment().format(" (DD/MM/YYYY) ");
 
-        
+
             $("#results-display").append(cityName + date);
             $("#weather-icon").attr("src", weatherIconURL);
             $("#temperature").append("Temp: " + temperature + "\xB0C");
             $("#wind").append("Wind: " + wind + " km/h");
             $("#humidity").append("Humidity: " + humidity + "%");
-            $("#uv-index").append("UV Index: " + uvIndex);
+
 
             var longitude = data.coord.lon;
             var latitude = data.coord.lat;
 
-            var uvIndexURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=" +  APIKey + "&exclude=minutely,hourly";
+            var uvIndexURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
             fetch(uvIndexURL)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (data) {
                     console.log(data);
+                    var uvIndex = data.value;
+                    $("#uv-index").append("UV Index: " + uvIndex);
                 })
 
         })
 }
 
 // Function to generate 5 dates from current day using moment
-function returnFiveDayDate () {
+function returnFiveDayDate() {
     // var dayOne = moment().add(1, "day").format("DD/MM/YYYY");
     var dayTwo = moment().add(2, "days").format("DD/MM/YYYY");
     var dayThree = moment().add(3, "days").format("DD/MM/YYYY");
     var dayFour = moment().add(4, "days").format("DD/MM/YYYY");
     var dayFive = moment().add(5, "days").format("DD/MM/YYYY");
-    
+
     // $("#first-day").prepend(dayOne);
     $("#second-day").prepend(dayTwo);
     $("#third-day").prepend(dayThree);
@@ -90,91 +93,104 @@ function returnFiveDayDate () {
 }
 
 
-function returnResultsFiveDay () {
+function returnResultsFiveDay() {
     userCityEntry = $("#user-input").val()
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCityEntry + "&appid=" + APIKey + "&units=metric";
     fetch(queryURL)
         .then(function (response) {
-                return response.json();
-            })
-        .then(function (data){
+            return response.json();
+        })
+        .then(function (data) {
             console.log(data.list)
             var fiveDayForecastEls = document.querySelectorAll(".five-day");
             for (i = 0; i < fiveDayForecastEls.length; i++) {
                 fiveDayForecastEls[i].innerHTML = "";
-                
+
                 var forecastIndex = i + 4;
-                console.log(forecastIndex)
+                // console.log(forecastIndex)
                 var weatherIconCode = (data.list[forecastIndex].weather[0].icon);
+                console.log(weatherIconCode);
                 var weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIconCode + ".png"
+                console.log(weatherIconURL)
                 var futureForecastTemperature = (data.list[forecastIndex].main.temp);
                 var futureForecastWind = (data.list[forecastIndex].wind.speed);
                 var futureForecastHumidity = (data.list[forecastIndex].main.humidity);
                 var dayOne = moment().add(1, "day").format("DD/MM/YYYY");
-            
-                        
-                $("#first-day").append(dayOne)
-                $("#weather-icon-first").attr("src", weatherIconURL)
-                $("#first-day").append(   
-                    "</br>" +
-                    "</br>" + "Temp: " +
-                    futureForecastTemperature + 
-                    "</br>" + "Wind: " +
-                    futureForecastWind + 
-                    "</br>" + "Humidity: " +
+
+
+                $("#first-day").append(dayOne);
+                $("#weather-icon-first").attr("src", weatherIconURL);
+                $("#first-day").append(
+                    "<br>" + "<br>" + "Temp: " +
+                    futureForecastTemperature +
+                    "<br>" + "<br>" + "Wind: " +
+                    futureForecastWind +
+                    "<br>" + "<br>" + "Humidity: " +
                     futureForecastHumidity);
 
                 forecastIndex = i + 12;
-                    futureForecastTemperature = (data.list[forecastIndex].main.temp);
-                    futureForecastWind = (data.list[forecastIndex].wind.speed);
-                    futureForecastHumidity = (data.list[forecastIndex].main.humidity);
+                weatherIconCode = (data.list[forecastIndex].weather[0].icon);
+                weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIconCode + ".png"
+                futureForecastTemperature = (data.list[forecastIndex].main.temp);
+                futureForecastWind = (data.list[forecastIndex].wind.speed);
+                futureForecastHumidity = (data.list[forecastIndex].main.humidity);
 
-                    $("#second-day").append( 
-                        "</br>" + "Temp: " +
-                        futureForecastTemperature + 
-                        "</br>" + "Wind: " +
-                        futureForecastWind + 
-                        "</br>" + "Humidity: " +
-                        futureForecastHumidity);
+                $("#weather-icon-first").attr("src", weatherIconURL);
+                $("#second-day").append(
+                    "<br>" + "<br>" + "Temp: " +
+                    futureForecastTemperature +
+                    "<br>" + "<br>" + "Wind: " +
+                    futureForecastWind +
+                    "<br>" + "<br>" + "Humidity: " +
+                    futureForecastHumidity);
 
                 forecastIndex = i + 20;
-                    futureForecastTemperature = (data.list[forecastIndex].main.temp);
-                    futureForecastWind = (data.list[forecastIndex].wind.speed);
-                    futureForecastHumidity = (data.list[forecastIndex].main.humidity);
-    
-                    $("#third-day").append( 
-                        "</br>" + "Temp: " +
-                        futureForecastTemperature + 
-                        "</br>" + "Wind: " +
-                        futureForecastWind + 
-                        "</br>" + "Humidity: " +
-                        futureForecastHumidity);
+                weatherIconCode = (data.list[forecastIndex].weather[0].icon);
+                weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIconCode + ".png"
+                futureForecastTemperature = (data.list[forecastIndex].main.temp);
+                futureForecastWind = (data.list[forecastIndex].wind.speed);
+                futureForecastHumidity = (data.list[forecastIndex].main.humidity);
+
+                $("#weather-icon-first").attr("src", weatherIconURL);
+                $("#third-day").append(
+                    "<br>" + "<br>" + "Temp: " +
+                    futureForecastTemperature +
+                    "<br>" + "<br>" + "Wind: " +
+                    futureForecastWind +
+                    "<br>" + "<br>" + "Humidity: " +
+                    futureForecastHumidity);
 
                 forecastIndex = i + 28;
-                    futureForecastTemperature = (data.list[forecastIndex].main.temp);
-                    futureForecastWind = (data.list[forecastIndex].wind.speed);
-                    futureForecastHumidity = (data.list[forecastIndex].main.humidity);
-        
-                    $("#fourth-day").append( 
-                        "</br>" + "Temp: " +
-                        futureForecastTemperature + 
-                        "</br>" + "Wind: " +
-                        futureForecastWind + 
-                        "</br>" + "Humidity: " +
-                        futureForecastHumidity);
+                weatherIconCode = (data.list[forecastIndex].weather[0].icon);
+                weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIconCode + ".png"
+                futureForecastTemperature = (data.list[forecastIndex].main.temp);
+                futureForecastWind = (data.list[forecastIndex].wind.speed);
+                futureForecastHumidity = (data.list[forecastIndex].main.humidity);
+
+                $("#weather-icon-first").attr("src", weatherIconURL);
+                $("#fourth-day").append(
+                    "<br>" + "<br>" + "Temp: " +
+                    futureForecastTemperature +
+                    "<br>" + "<br>" + "Wind: " +
+                    futureForecastWind +
+                    "<br>" + "<br>" + "Humidity: " +
+                    futureForecastHumidity);
 
                 forecastIndex = i + 36;
-                    futureForecastTemperature = (data.list[forecastIndex].main.temp);
-                    futureForecastWind = (data.list[forecastIndex].wind.speed);
-                    futureForecastHumidity = (data.list[forecastIndex].main.humidity);
-    
-                    $("#fifth-day").append( 
-                        "</br>" + "Temp: " +
-                        futureForecastTemperature + 
-                        "</br>" + "Wind: " +
-                        futureForecastWind + 
-                        "</br>" + "Humidity: " +
-                        futureForecastHumidity);
+                weatherIconCode = (data.list[forecastIndex].weather[0].icon);
+                weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIconCode + ".png"
+                futureForecastTemperature = (data.list[forecastIndex].main.temp);
+                futureForecastWind = (data.list[forecastIndex].wind.speed);
+                futureForecastHumidity = (data.list[forecastIndex].main.humidity);
+
+                $("#weather-icon-first").attr("src", weatherIconURL);
+                $("#fifth-day").append(
+                    "<br>" + "<br>" + "Temp: " +
+                    futureForecastTemperature +
+                    "<br>" + "<br>" + "Wind: " +
+                    futureForecastWind +
+                    "<br>" + "<br>" + "Humidity: " +
+                    futureForecastHumidity);
                 return
             }
         })
